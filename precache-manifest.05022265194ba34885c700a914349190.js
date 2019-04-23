413e9c0342d7a2d0d88176fb71b64b73d1806148
Abhishek <abhishek.aapte@gmail.com>
2019-04-23 18:59:26 +0530

    Updates

---
+++ precache-manifest.05022265194ba34885c700a914349190.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaabc2de7a0af70cd5ce",
+    "url": "/github-username/static/js/runtime~main.73108f0a.js"
+  },
+  {
+    "revision": "dc4721ebcabc304b8d7d",
+    "url": "/github-username/static/js/main.42066be7.chunk.js"
+  },
+  {
+    "revision": "f210776cf63abca1f006",
+    "url": "/github-username/static/js/2.6f71932c.chunk.js"
+  },
+  {
+    "revision": "dc4721ebcabc304b8d7d",
+    "url": "/github-username/static/css/main.07019aa7.chunk.css"
+  },
+  {
+    "revision": "c8b048307e6e57948fb9a8a41bb70207",
+    "url": "/github-username/index.html"
+  }
+];
